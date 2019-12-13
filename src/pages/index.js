3d03f48f4f71827bff72d4d9550e822386174741
Jean Zhang <jean.nmn.zhang@gmail.com>
2019-12-13 15:37:52 -0500

    typing effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,25 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import styled from "styled-components"
 import { PageWrapper } from "../components/basecomponents"
+import Typist from 'react-typist'
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  const [typingDone, setTypingDone] = React.useState(false);
+  return <Layout>
     <SEO title="Home" />
     <HomepageWrapper>
-      <TagLine>Hi! <UnderlineRed><BlackTextWrapper>I'm Jean</BlackTextWrapper></UnderlineRed>, a software developer and third year student at Northeastern University. </TagLine>
+      <TextWrapper>
+        <Typist cursor={{show: false}} avgTypingDelay={50}>
+          <TagLine>Hi! <UnderlineRed><BlackTextWrapper>I'm Jean</BlackTextWrapper></UnderlineRed>,
+          a software developer and third year student at Northeastern University. </TagLine>
+        </Typist>
+      </TextWrapper>
       <ImageWrapper>
-        <Image imageName="profile"/>
+        <Image/>
       </ImageWrapper>
     </HomepageWrapper>
   </Layout>
-)
+}
 
 const HomepageWrapper = styled(PageWrapper)`
   display: flex;
@@ -30,14 +37,15 @@ const HomepageWrapper = styled(PageWrapper)`
 `;
 
 const ImageWrapper = styled.div`
-  width: 100%;
+  width: 80%;
   margin-bottom: 1.45rem;
-  @media (max-width: 768px) {
-    width: 80%;
-  }
   @media (max-width: 420px) {
     width: 90%;
   }
+`;
+
+const TextWrapper = styled.div`
+  width: 200%;
 `;
 
 const TagLine = styled.h1`
